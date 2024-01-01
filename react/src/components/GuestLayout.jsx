69c0ed3958a4f9e9b5f7e1e2a3a36be332db0e44
Adolphus Miracle <miraboy13@gmail.com>
2024-01-01 21:58:26 +0100

    feat: created component folder and different layout component

---
+++ react/src/components/GuestLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Outlet} from "react-router-dom";
+
+const GuestLayout = () => {
+    return (
+     <div>
+         GuestLayout
+         <Outlet />
+     </div>
+  )
+}
+
+export default GuestLayout
