eef2d5a634210865626b267d01fb798d9785d028
Adolphus Miracle <miraboy13@gmail.com>
2024-01-01 22:29:10 +0100

    feat: use contextProvider to check if token is set

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import {Outlet} from "react-router-dom";
+import {Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 const DefaultLayout = () => {
+    const {token, user} = useStateContext()
+    if (!token){
+        return  <Navigate to={'/login' } />
+    }
     return (
      <div>
          DefaultLayout
