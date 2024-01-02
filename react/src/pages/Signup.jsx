180299bd6bc0a0eb2acb1f6985d49fec52daacfb
Adolphus Miracle <miraboy13@gmail.com>
2024-01-02 17:53:29 +0100

    feat: designed signup page

--- react/src/pages/Signup.jsx
+++ react/src/pages/Signup.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Signup = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
-     <div>
-        Signup
-     </div>
-  )
+        <div className="login-signup-form fadeInDown animated">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">
+                        Signup Now
+                    </h1>
+                    <input type="text" placeholder="Name"/>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <input type="password" placeholder="Confirm Password"/>
+                    <button className="btn btn-block">Signup</button>
+                    <p className="message">
+                        Have an account? <Link to="/login" >Login here</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Signup
