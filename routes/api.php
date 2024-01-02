34319e1b511b40f2372c91883bc3cae99d80a61b
Adolphus Miracle <miraboy13@gmail.com>
2024-01-03 00:22:58 +0100

    feat: defined route for users crud functionality

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +20,9 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::apiResource('users',UsersController::class);
     Route::post('logout',[UserController::class,'logout']);
 });
 Route::post('signup',[UserController::class,'signup']);
 Route::post('login',[UserController::class,'login']);
-Route::get('/test',function (){
-   return response()->json(['message'=>'working'],200);
-});
+
