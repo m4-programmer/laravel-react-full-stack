5b91086ceb1033eb5b456488ebb3046c637bdf26
Adolphus Miracle <miraboy13@gmail.com>
2024-01-02 22:53:38 +0100

    feat: implemented api call and state management

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Link} from "react-router-dom";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 const Login = () => {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const {setUser,setToken} = useStateContext()
+    const [error, _setError] = useState(null)
+
+    const setError = error => {
+        _setError(error)
+        setTimeout(()=>{_setError(null)}, 4000)
+    }
     const onSubmit = (e) => {
         e.preventDefault();
+        //we get the user inputs
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        }
+        console.log(payload)
+        //we send it to the server
+        axiosClient.post('/login', payload).then(({data})=>{
+            setUser(data.user)
+            setToken(data.token)
+        }).catch((err)=>{
+            const {response} = err
+
+            if (response && response.status === 404){
+                setError({message: response.data.message})
+
+            }
+            if (response && response.status === 422){
+                setError(response.data.errors)
+
+            }
+        })
     }
     return (
         <div className="login-signup-form fadeInDown animated">
@@ -12,8 +45,11 @@ const Login = () => {
                     <h1 className="title">
                         Login into your account
                     </h1>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
+                    <input ref={emailRef} type="email" placeholder="Email"/>
+                    {(error && error.email) && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.email}</p>}
+                    <input ref={passwordRef} type="password" placeholder="Password"/>
+                    {(error && error.password) && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.password}</p>}
+                    {(error && error.message) && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.message}</p>}
                     <button className="btn btn-block">Login</button>
                     <p className="message">
                         Not Registered? <Link to="/signup" >Create an account</Link>
--- react/src/pages/Signup.jsx
+++ react/src/pages/Signup.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Link} from "react-router-dom";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 const Signup = () => {
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const confirmPasswordRef = useRef()
+    const [error, _setError] = useState(null);
+    const {setToken,setUser} = useStateContext()
+
+    const setError = error => {
+        _setError(error)
+        setTimeout(()=>{_setError(null)}, 4000)
+    }
     const onSubmit = (e) => {
         e.preventDefault();
+        const payload  = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: confirmPasswordRef.current.value,
+        }
+        axiosClient.post('/signup', payload).then(({data})=>{
+            setUser(data.user)
+            setToken(data.token)
+        }).catch(error => {
+            const response = error.response
+            if (response && response.status === 422){
+                setError(response.data.errors)
+                console.log(response.data.errors);
+            }
+        })
     }
     return (
         <div className="login-signup-form fadeInDown animated">
@@ -12,10 +41,14 @@ const Signup = () => {
                     <h1 className="title">
                         Signup Now
                     </h1>
-                    <input type="text" placeholder="Name"/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <input type="password" placeholder="Confirm Password"/>
+                    <input ref={nameRef} type="text" placeholder="Name"/>
+                    {error && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.name}</p>}
+
+                    <input ref={emailRef} type="email" placeholder="Email"/>
+                    {error && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.email}</p>}
+                    <input ref={passwordRef} type="password" placeholder="Password"/>
+                    {error && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.password}</p>}
+                    <input ref={confirmPasswordRef} type="password" placeholder="Confirm Password"/>
                     <button className="btn btn-block">Signup</button>
                     <p className="message">
                         Have an account? <Link to="/login" >Login here</Link>
