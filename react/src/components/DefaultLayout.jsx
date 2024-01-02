dcacb388c39dd3edeaf85e6e1ed4bdcc9e081fe9
Adolphus Miracle <miraboy13@gmail.com>
2024-01-02 17:52:24 +0100

    feat: designed default page

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import {Navigate, Outlet} from "react-router-dom";
+import {Link, Navigate, Outlet} from "react-router-dom";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 const DefaultLayout = () => {
-    const {token, user} = useStateContext()
+    const {token, user, setToken} = useStateContext()
     if (!token){
         return  <Navigate to={'/login' } />
     }
+
+    function handleLogout(e) {
+        e.preventDefault();
+        setToken("");
+        console.log("User Logged out")
+    }
+
     return (
-     <div>
-         DefaultLayout
-         <Outlet />
+     <div id={"defaultLayout"}>
+         <aside>
+             <Link to='/dashboard' >Dashboard</Link>
+             <Link to='/users' >Users</Link>
+         </aside>
+         <div className="content">
+             <header>
+                 <div>
+                     Header
+                 </div>
+                 <div>
+                     {user.name ?? "N/A"}
+                     <a href="#" onClick={handleLogout} className="btn-logout">Logout</a>
+                 </div>
+             </header>
+             <main>
+                 <Outlet />
+             </main>
+         </div>
+
      </div>
   )
 }
