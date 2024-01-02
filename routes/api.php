512f065694903065a465cd051d780dfdd3d851ac
Adolphus Miracle <miraboy13@gmail.com>
2024-01-02 22:54:33 +0100

    feat: defined routes for backend

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('logout',[UserController::class,'logout']);
+});
+Route::post('signup',[UserController::class,'signup']);
+Route::post('login',[UserController::class,'login']);
+Route::get('/test',function (){
+   return response()->json(['message'=>'working'],200);
 });
