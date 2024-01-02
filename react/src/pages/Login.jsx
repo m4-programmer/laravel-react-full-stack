d17805fdd10b808bdbd0f8f9e2f0fa4624dda142
Adolphus Miracle <miraboy13@gmail.com>
2024-01-02 17:53:05 +0100

    feat: designed login page

--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Login = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            Login
+        <div className="login-signup-form fadeInDown animated">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">
+                        Login into your account
+                    </h1>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Not Registered? <Link to="/signup" >Create an account</Link>
+                    </p>
+                </form>
+            </div>
         </div>
     )
 }
