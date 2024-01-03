cdf47198d188f5ff43cbbeec18d3f87ea9cd95b5
Adolphus Miracle <miraboy13@gmail.com>
2024-01-03 19:02:12 +0100

    feat: implemented logic to create new user and update new user

---
+++ react/src/pages/UserForm.jsx
@@ -0,0 +1,109 @@
+import React, {useState, useRef, useEffect} from 'react';
+import {Link, useParams,useNavigate } from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+import axiosClient from "../axios-client.js";
+
+const UserForm = () => {
+    const [loading, setLoading] = useState(false)
+    const {setNotification} = useStateContext()
+    const {id} = useParams();
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState({
+        id: null,
+        name: null,
+        email: null,
+        password: null,
+        password_confirmation: null,
+    });
+    console.log(user)
+    const getUser = id => {
+        if (id) {
+            setLoading(true)
+            axiosClient.get('/users/' + id).then(({data}) => {
+                setLoading(false)
+                setUser(data)
+                console.log(data)
+            })
+            return true;
+        }
+    }
+    useEffect(()=>{
+        getUser(id)
+    },[])
+
+    const [error, _setError] = useState(null);
+
+    const setError = error => {
+        _setError(error)
+        setTimeout(()=>{_setError(null)}, 4000)
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const payload  = {
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            password_confirmation: user.password_confirmation,
+        }
+        //we update the user records
+        setLoading(true)
+        if (id){
+
+            axiosClient.put('/users/'+id, payload).then(({data})=>{
+                setLoading(false)
+                setNotification("User Updated Successfully")
+            }).catch(error => {
+                setLoading(false)
+                const response = error.response
+                if (response && response.status === 422){
+                    setError(response.data.errors)
+                    console.log(response.data.errors);
+                }
+            })
+        }else{
+            //we create a new user
+            axiosClient.post('/users', payload).then(({data})=>{
+                console.log(data)
+                setLoading(false)
+                setNotification("User Created Successfully")
+                return navigate('/users')
+            }).catch(error => {
+                setLoading(false)
+                const response = error.response
+                if (response && response.status === 422){
+                    setError(response.data.errors)
+                    console.log(response.data.errors);
+                }
+            })
+        }
+
+    }
+    return (
+        //write condition that will display h1 of user name or a text New user
+     <div className='card fadeInDown animated'>
+         {loading && (<div className="text-center"><h3>Loading...</h3></div>)}
+         {/*Show the form , and display errors, i can copy forms from signup page and it should show if loading is false*/}
+         <div className="form">
+             <form onSubmit={onSubmit}>
+                 <h1 className="title ">
+                     { id ? user?.name : "New User" }
+                 </h1><br/>
+                 <input onChange={(e)=>{setUser({...user, name: e.target.value})}} value={user.name ?? ''} type="text" placeholder="Name"/>
+                 {error && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.name}</p>}
+
+                 <input onChange={(e)=>{setUser({...user, email: e.target.value})}} value={user.email ?? ''} type="email" placeholder="Email"/>
+                 {error && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.email}</p>}
+                 {/*Password fields will show conditionally*/}
+                 <input onChange={(e)=>{setUser({...user, password: e.target.value})}}  type="password" placeholder="Password"/>
+                 {(error && error.password) && <p style={{color: 'red',marginTop: '-15px',paddingBottom: '5px'}}>{error?.password}</p>}
+                 <input onChange={(e)=>{setUser({...user, password_confirmation: e.target.value})}}  type="password" placeholder="Confirm Password"/>
+                 <button className="btn btn-block">{ id ? "Update" : "Save"}</button>
+
+             </form>
+         </div>
+     </div>
+  )
+}
+
+export default UserForm
